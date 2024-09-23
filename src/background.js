4515c8ee355ee494c34788d9eb86ee20a8041512
Ramiro Espada <respada@apple.com>
2024-09-23 17:06:23 -0300

    feat: refactor, fixed issue with overlay labels, added video elements

--- src/background.js
+++ src/background.js
@@ -32,14 +32,17 @@ function injectedFunction() {
 
   const createLabel = (content, top, left, bottom, right, transform) => {
     let label = document.createElement("div");
-    label.classList.add("RE-label");
+    label.classList.add("web-skeleton-label");
 
     text = document.createTextNode(content);
+		label.setAttribute("web-skeleton-label", content);
     label.style.position = "absolute";
     label.style.top = top;
     label.style.left = left;
     label.style.right = right;
     label.style.bottom = bottom;
+		label.style.width = "auto";
+		label.style.height = "auto";
     label.style.fontFamily = "-apple-system, Helvetica, Arial, sans-serif";
     label.style.fontWeight = "bold";
     label.style.letterSpacing = "1px";
@@ -101,7 +104,7 @@ function injectedFunction() {
 
         if (displayLines) {
           line = document.createElement("div");
-          line.classList.add("RE-line-top");
+          line.classList.add("web-skeleton-line-top");
           line.style.position = "absolute";
           line.style.top = "-1px";
           line.style.left = "calc(100% + 1px)";
@@ -112,7 +115,7 @@ function injectedFunction() {
           target.appendChild(line);
 
           line = document.createElement("div");
-          line.classList.add("RE-line-top");
+          line.classList.add("web-skeleton-line-top");
           line.style.position = "absolute";
           line.style.top = "-1px";
           line.style.left = "calc(-100vw - 1px)";
@@ -123,7 +126,7 @@ function injectedFunction() {
           target.appendChild(line);
 
           line = document.createElement("div");
-          line.classList.add("RE-line-bottom");
+          line.classList.add("web-skeleton-line-bottom");
           line.style.position = "absolute";
           line.style.top = "100%";
           line.style.left = "calc(100% + 1px)";
@@ -134,7 +137,7 @@ function injectedFunction() {
           target.appendChild(line);
 
           line = document.createElement("div");
-          line.classList.add("RE-line-bottom");
+          line.classList.add("web-skeleton-line-bottom");
           line.style.position = "absolute";
           line.style.top = "100%";
           line.style.left = "calc(-100vw - 1px)";
@@ -149,8 +152,8 @@ function injectedFunction() {
 
         if (displayOverlay) {
           overlay = document.createElement("div");
-          overlay.classList.add("RE-overlay");
-          overlay.classList.add("RE-overlay-" + overlayTarget);
+          overlay.classList.add("web-skeleton-overlay");
+          overlay.classList.add("web-skeleton-overlay-" + overlayTarget);
           overlay.style.position = "absolute";
           overlay.style.top = "0";
           overlay.style.left = "0";
@@ -175,7 +178,7 @@ function injectedFunction() {
 
           if (displayLabel) {
             label = createLabel(
-              "",
+              query,
               "50%",
               "50%",
               "auto",
@@ -199,7 +202,7 @@ function injectedFunction() {
           }
         }
       }
-      target.classList.add("RE-web-skeleton");
+      target.classList.add("web-skeleton");
       target.style.outline = "1px " + color + " solid";
     }
   };
@@ -219,19 +222,19 @@ function injectedFunction() {
   };
 
   const removeDebugDecoration = () => {
-    removeElementsByClass("RE-web-skeleton", false);
-    removeElementsByClass("RE-overlay", true);
-    removeElementsByClass("RE-label", true);
-    removeElementsByClass("RE-line-top", true);
-    removeElementsByClass("RE-line-bottom", true);
+    removeElementsByClass("web-skeleton", false);
+    removeElementsByClass("web-skeleton-overlay", true);
+    removeElementsByClass("web-skeleton-label", true);
+    removeElementsByClass("web-skeleton-line-top", true);
+    removeElementsByClass("web-skeleton-line-bottom", true);
   };
 
   const updateOverlaysLabel = () => {
-    const domElements = document.getElementsByClassName("RE-overlay");
+    const domElements = document.getElementsByClassName("web-skeleton-overlay");
     for (let i = 0; i < domElements.length; i++) {
       let target = domElements[i];
       let size = target.getBoundingClientRect();
-      let label = target.getElementsByClassName("RE-label")[0];
+      let label = target.getElementsByClassName("web-skeleton-label")[0];
       if (label) {
         label.textContent =
           Math.round(size.width) + "x" + Math.round(size.height);
@@ -253,7 +256,7 @@ function injectedFunction() {
       document.documentElement.clientHeight || 0,
       window.innerHeight || 0
     );
-    const label = body.getElementsByClassName("RE-viewport-label")[0];
+    const label = body.getElementsByClassName("web-skeleton-viewport-label")[0];
     if (label) {
       let content = "XS";
       if (vw > 480) {
@@ -282,18 +285,18 @@ function injectedFunction() {
     const body = document.getElementsByTagName("body")[0];
     let state, viewportLabel;
 
-    if (body.getAttribute("RE-WebSkeleton")) {
-      if (body.getElementsByClassName("RE-viewport-label")) {
-        viewportLabel = body.getElementsByClassName("RE-viewport-label")[0];
+    if (body.getAttribute("WebSkeleton")) {
+      if (body.getElementsByClassName("web-skeleton-viewport-label")) {
+        viewportLabel = body.getElementsByClassName("web-skeleton-viewport-label")[0];
         body.removeChild(viewportLabel);
       }
       window.removeEventListener("resize", onResizeHandler);
       window.removeEventListener("scroll", onScrollHandler);
-      body.removeAttribute("RE-WebSkeleton");
+      body.removeAttribute("WebSkeleton");
       state = "remove";
     } else {
       viewportLabel = document.createElement("div");
-      viewportLabel.classList.add("RE-viewport-label");
+      viewportLabel.classList.add("web-skeleton-viewport-label");
       viewportLabel.style.userSelect = "none";
       viewportLabel.style.position = "fixed";
       viewportLabel.style.bottom = "15px";
@@ -316,7 +319,7 @@ function injectedFunction() {
       onResizeHandler();
 
       body.style.overflowX = "hidden";
-      body.setAttribute("RE-WebSkeleton", "true");
+      body.setAttribute("WebSkeleton", "true");
       state = "add";
     }
 
@@ -365,7 +368,6 @@ function injectedFunction() {
       displayLines: false,
       displayOverlay: false,
     },
-
     {
       query: "h1",
       type: "query",
@@ -447,13 +449,22 @@ function injectedFunction() {
       displayOverlay: true,
       overlayTarget: "parent",
     },
+		{
+      query: "video",
+      type: "query",
+      color: color2,
+      displayLabel: false,
+      displayLines: false,
+      displayOverlay: true,
+      overlayTarget: "parent",
+    },
     {
       query: "figure",
       type: "query",
       color: color2,
       displayLabel: false,
       displayLines: false,
-      displayOverlay: true,
+      displayOverlay: false,
       overlayTarget: "self",
     },
     {
